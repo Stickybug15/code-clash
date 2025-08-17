568f7a95c49951ac5d0309be82fba7db40abc24b
NexushasTaken <nexushastaken@gmail.com>
2025-08-17 23:28:18 +0800

    better authentication

--- scripts/AuthenticationMenu.gd
+++ scripts/AuthenticationMenu.gd
@@ -3,8 +3,8 @@ extends HBoxContainer
 @onready var email_signin_edit := $SignInMenu/GridContainer/EmailEdit
 @onready var pass_signin_edit  := $SignInMenu/GridContainer/PasswordEdit
 
-@onready var email_signup_edit := $SignInMenu/GridContainer/EmailEdit
-@onready var pass_signup_edit  := $SignInMenu/GridContainer/PasswordEdit
+@onready var email_signup_edit := $SignUpMenu/GridContainer/EmailEdit
+@onready var pass_signup_edit  := $SignUpMenu/GridContainer/PasswordEdit
 
 
 func hide_all():
@@ -12,43 +12,59 @@ func hide_all():
 		menu.visible = false
 
 
-func show_main_menu(name: String):
-	hide_all()
-	$MainMenu.visible = true
-	$MainMenu/Status.text = name
-
-
 func _on_sign_in_btn_pressed() -> void:
-	var auth_task: AuthTask = await Supabase.auth.sign_in(email_signin_edit.text, pass_signin_edit.text).completed
-	print(auth_task.user)
-
-	hide_all()
-	show_main_menu(auth_task.user.email)
+	Supabase.auth.error.connect(
+		func(error: SupabaseAuthError):
+			$SignInMenu/GridContainer/StatusLabel.text = error.message
+	)
+	Supabase.auth.signed_in.connect(
+		func(user: SupabaseUser):
+			print(user)
+			hide_all()
+			$MainMenu.visible = true
+	)
+	Supabase.auth.sign_in(email_signin_edit.text, pass_signin_edit.text)
 
 
 func _on_sign_up_btn_pressed() -> void:
-	var auth_task: AuthTask = await Supabase.auth.sign_up(email_signup_edit.text, pass_signup_edit.text).completed
-	print(Supabase.auth.client)
-
-	hide_all()
-	show_main_menu(auth_task.user.email)
+	Supabase.auth.error.connect(
+		func(error: SupabaseAuthError):
+			$SignUpMenu/GridContainer/StatusLabel.text = error.message
+	)
+	Supabase.auth.signed_up.connect(
+		func(user: SupabaseUser):
+			print(user)
+			hide_all()
+			$MainMenu.visible = true
+	)
+	print(email_signup_edit.text, pass_signup_edit.text)
+	Supabase.auth.sign_up(email_signup_edit.text, pass_signup_edit.text)
 
 
 func _on_sign_in_annonymous_pressed() -> void:
-	print("Signed In as Anonymous is pressed")
-	var auth_task: AuthTask = await Supabase.auth.sign_in_anonymous().completed
-	print(auth_task.user)
-
-	hide_all()
-	show_main_menu("Anonymous")
+	Supabase.auth.error.connect(
+		func(error: SupabaseAuthError):
+			$AccessModeMenu/StatusLabel.text = error.message
+	)
+	Supabase.auth.signed_in_anonymous.connect(
+		func():
+			hide_all()
+			$MainMenu.visible = true
+	)
+	Supabase.auth.sign_in_anonymous()
 
 
 func _on_sign_out_btn_pressed() -> void:
-	var auth_task: AuthTask = await Supabase.auth.sign_out().completed
-	print(auth_task.user)
-
-	hide_all()
-	$AccessModeMenu.visible = true
+	Supabase.auth.error.connect(
+		func(error: SupabaseAuthError):
+			$MainMenu/StatusLabel.text = error.message
+	)
+	Supabase.auth.signed_out.connect(
+		func():
+			hide_all()
+			$AccessModeMenu.visible = true
+	)
+	Supabase.auth.sign_out()
 
 
 func _on_quit_btn_pressed() -> void:
@@ -68,3 +84,19 @@ func _on_goto_sign_in_menu_btn_pressed() -> void:
 func _on_goto_sign_up_menu_btn_pressed() -> void:
 	hide_all()
 	$SignUpMenu.visible = true
+
+
+func _on_sign_up_menu_visibility_changed() -> void:
+	$SignUpMenu/GridContainer/StatusLabel.text = ""
+
+
+func _on_sign_in_menu_visibility_changed() -> void:
+	$SignInMenu/GridContainer/StatusLabel.text = ""
+
+
+func _on_main_menu_visibility_changed() -> void:
+	if $MainMenu.visible:
+		if Supabase.auth.client.is_anonymous:
+			$MainMenu/AuthStatus.text = "Signed In as Anonymous"
+		else:
+			$MainMenu/AuthStatus.text = "Signed In as " + Supabase.auth.client.email
