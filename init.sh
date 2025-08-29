3cdf17f54cad4687038fc6cfa5294902f591e14a
NexushasTaken <nexushastaken@gmail.com>
2025-08-30 07:02:01 +0800

    fix typo

--- init.sh
+++ init.sh
@@ -77,7 +77,7 @@ init_compiler() {
   local pacman="/c/msys64/usr/bin/pacman"
   $pacman -S mingw-w64-x86_64-gcc mingw-w64-i686-gcc --noconfirm --needed
 
-  ./just.sh build_library build-windows-x86_64
+  ./just.sh build-library build-windows-x86_64
   ./just.sh build-windows-x86_64
 }
 
