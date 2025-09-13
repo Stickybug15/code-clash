3ca1e1f3d6d7a9d42f784cec4dcae8fa975d7839
NexushasTaken <nexushastaken@gmail.com>
2025-09-13 22:11:39 +0800

    fix incorrect scenes location

--- godot/scripts/player_name.gd
+++ godot/scripts/player_name.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://play_role_start.tscn")
+	get_tree().change_scene_to_file("res://godot/scenes/play_role_start.tscn")
--- godot/scripts/sign_in_menu.gd
+++ godot/scripts/sign_in_menu.gd
@@ -3,4 +3,4 @@ extends Control
 
 
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://logs_menu.tscn")
+	get_tree().change_scene_to_file("res://godot/scenes/logs_menu.tscn")
