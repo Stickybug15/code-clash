8781db507a55c0693b20ce9cee09bdf265d06a64
KimpeeA <k1mpeeaton06@gmail.com>
2025-09-13 17:50:43 +0800

    fix godot doesn't startup

--- godot.sh
+++ godot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$OS" = "Windows_NT" ]]; then
-  exec "./.cache/Godot_v4.4.1-stable_win64.exe" -e --path $PWD $@
+  exec "./.cache/Godot_v4.4.1-stable_win64.exe" -e --path "$PWD" $@
 else
   if type godot4; then
     exec godot4 -e --path $PWD $@
