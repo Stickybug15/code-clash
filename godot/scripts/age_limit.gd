4ca3d3f5a40b0c901a873c51f5c9ccdd9bfed8f2
KimpeeA <k1mpeeaton06@gmail.com>
2025-09-13 18:07:44 +0800

    move scenes to godot/scenes

--- godot/scripts/age_limit.gd
+++ godot/scripts/age_limit.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_yes_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://logs_menu.tscn")
+	get_tree().change_scene_to_file("res://godot/scenes/logs_menu.tscn")
 
 
 
