6ca7c4a8ea45d03bc998d1c6fdb20862b4df2dc4
NexushasTaken <nexushastaken@gmail.com>
2025-09-16 13:50:54 +0800

    fix launching Godot using ./godot.sh doesn't work

--- godot.sh
+++ godot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$OS" = "Windows_NT" ]]; then
-  exec "./.cache/Godot_v*.exe" -e --path "$PWD" $@
+  exec "./.cache/Godot_v4.5-stable_win64.exe" -e --path "$PWD" $@
 else
   exec godot -e --path $PWD $@
 fi
