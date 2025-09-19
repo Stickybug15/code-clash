415d6ee665bb31f0d2b801c81c3beb91844baf29
NexushasTaken <nexushastaken@gmail.com>
2025-09-19 12:24:20 +0800

    refactored authentication menus

--- godot/scripts/sign_in_menu.gd
+++ godot/scripts/sign_in_menu.gd
@@ -1,13 +1,12 @@
-extends Control
+extends CanvasLayer
+
+@onready
+var hover_sfx: AudioStreamPlayer = $hover_fx
 
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://godot/scenes/authentication_menu.tscn")
 
 
-func _on_back_button_mouse_entered() -> void:
-	$TouchScreenButton/hover_fx.play()
-
-
-func _on_login_button_mouse_entered() -> void:
-	$TouchScreenButton/hover_fx.play()
+func _on_mouse_entered() -> void:
+	hover_sfx.play()
