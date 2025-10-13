250e66d1c606f92a1c7903e7cb14468c4fae6838
NexushasTaken <nexushastaken@gmail.com>
2025-10-13 05:39:54 +0000

    add archlinux for ./init.sh

--- init.sh
+++ init.sh
@@ -142,7 +142,7 @@ init_compiler() {
   local pacman="/c/msys64/usr/bin/pacman"
   "$pacman" -S mingw-w64-x86_64-gcc mingw-w64-i686-gcc --noconfirm --needed
 
-  ./just.sh --set build_library yes build-windows-x86_64
+  ./just.sh build_library=yes build-windows-x86_64
 }
 
 open() {
@@ -156,6 +156,13 @@ if [[ "$OS" = "Windows_NT" ]]; then
   init_godot
   init_scons
   init_compiler
+else
+  source /etc/os-release
+  if [[ $ID = "arch" ]]; then
+    sudo pacman -S --needed gcc just scons ccache
+  fi
+
+  ./just.sh build_library=yes build-linux
 fi
 
 echo
