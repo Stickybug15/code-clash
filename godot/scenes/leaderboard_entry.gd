8e3b9981f2b67cd3f51eb1c7b8eabb0e4ee9df30
Stickybug15 <pantuaryan15@gmail.com>
2025-10-23 22:06:18 +0800

    WIP: null leaderboard

--- godot/scenes/leaderboard_entry.gd
+++ godot/scenes/leaderboard_entry.gd
@@ -2,22 +2,26 @@ class_name LeaderboardEntry
 extends HBoxContainer
 
 @onready var player_name_label: Label = $Name
+@onready var level_name_label: Label = $Level
 @onready var speed_label: Label = $Speed
 @onready var accuracy_label: Label = $Accuracy
 
 var player_name: String = ""
+var level_name: String = ""
 var speed: float = 0.0
 var accuracy: float = 0.0
 
 func _ready() -> void:
 	label_update()
-func set_data(name_val: String, speed_val: float, accuracy_val: float)->void:
+func set_data(name_val: String, level_val: String, speed_val: float, accuracy_val: float)->void:
 	player_name = name_val
+	level_name = level_val
 	speed = speed_val
 	accuracy = accuracy_val
 	label_update()
 
 func label_update() -> void:
 	player_name_label.text = player_name
+	level_name_label.text = level_name
 	speed_label.text = str(speed)
 	accuracy_label.text = str(accuracy)
--- godot/scenes/leaderboard_table.gd
+++ godot/scenes/leaderboard_table.gd
@@ -12,30 +12,32 @@ var user_pwd : String = "ryanpantua123"
 
 
 func _ready()->void:
-	fetch_leaderboard_async()
-func fetch_leaderboard_async() -> void:
+	fetch_leaderboard()
+
+func fetch_leaderboard() -> void:
 	print("======================================")
 	print("DEBUG: _fetch_leaderboard_async started")
 	var result : AuthTask = await Supabase.auth.sign_in(user_mail, user_pwd).completed
 	print("DEBUG: after login await")
 	if result.user != null:
-		print("failed!")
+		print("Login failed! Error")
 		return
 	print("login success!")
 	var query := SupabaseQuery.new().from("player_level_scores").select(["speed_seconds", "accuracy_score", "levels(name)","profiles(username)"])
 	var task2: DatabaseTask = await Supabase.database.query(query).completed
 	if task2.error == null:
 		print("Task2: ",task2.data)
+
 		for row_variant in task2.data:
 			var row : Dictionary = row_variant as Dictionary
 
 			#get username
-			var _user_name := ""
+			var player_name := ""
 			if row.has("profiles"):
 				var _profile_variant = row["profiles"]
 				if typeof((_profile_variant)) == TYPE_DICTIONARY:
 					var profiles: Dictionary = _profile_variant as Dictionary
-					_profile_variant = str(profiles.get("username: ", ""))
+					player_name = str(profiles.get("username", ""))
 
 			#get levels
 			var level_name := ""
@@ -49,21 +51,21 @@ func fetch_leaderboard_async() -> void:
 			var speed_val: float = row.get("speed_seconds", 0)
 			var accuracy_val: float = row.get("accuracy_score", 0)
 
-			var _speed := (float(speed_val)
-			if typeof(speed_val) in [TYPE_FLOAT, TYPE_INT] else 0.0)
+			var _speed := (float(speed_val) if typeof(speed_val) in [TYPE_FLOAT, TYPE_INT] else 0.0)
 
-			var _accuracy := (float(speed_val)
-			if typeof(accuracy_val) in [TYPE_FLOAT, TYPE_INT] else 0.0)
+			var _accuracy := (float(accuracy_val) if typeof(accuracy_val) in [TYPE_FLOAT, TYPE_INT] else 0.0)
 
-			print("Entry:", level_name, row["speed_seconds"], row["accuracy_score"])
-			add_entry(level_name, row["speed_seconds"], row["accuracy_score"])
+			print("Entry:", player_name, level_name, _speed, _accuracy)
+			#add_entry(_user_name, row["speed_seconds"], row["accuracy_score"])
+			add_entry(player_name,level_name,_speed,_accuracy)
 
 	else:
 		print("Error: ", task2.error)
 
-func add_entry(player_name: String, speed: float, accuracy: float) -> void:
+func add_entry(player_name: String,level_name: String, speed: float, accuracy: float) -> void:
 	var entry: LeaderboardEntry = entry_scene.instantiate()
 	entry.player_name = player_name
+	entry.level_name = level_name
 	entry.speed = speed
 	entry.accuracy = accuracy
 	entries_container.add_child(entry)
